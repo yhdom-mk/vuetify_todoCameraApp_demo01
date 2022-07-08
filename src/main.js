c141b4ed0662923cb5ce2ed70cc881d29770c721
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-09 01:49:14 +0900

    npm  material-design-icons-iconfont 追加

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 loadFonts()
 
