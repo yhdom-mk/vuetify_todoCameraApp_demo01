4e496c859bb988dc936a3893671cdcaf999af31e
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-11 21:25:45 +0900

    vuetify.js cssファイル icon property 追加 （仮）

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,14 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
+export default createVuetify({
+  icons: {
+    iconfont: 'mdi',
+  }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+})
