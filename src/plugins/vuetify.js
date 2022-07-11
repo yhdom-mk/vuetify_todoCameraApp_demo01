7042e2f513888528edb76fe8ff8d0f315d970e23
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-12 01:48:10 +0900

    import component, directives 追加保留

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,13 +2,19 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
 export default createVuetify({
-  icons: {
-    iconfont: 'mdi',
-  }
+  // components,
+  // directives,
+  // icons: {
+  //   iconfont: 'mdi',
+  // }
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 })
