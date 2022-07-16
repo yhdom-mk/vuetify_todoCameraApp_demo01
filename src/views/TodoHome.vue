d4730aa7f9e421c09e67555cf503667aafb25d8d
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-16 23:51:23 +0900

    checkbox 機能追加 error

--- src/views/TodoHome.vue
+++ src/views/TodoHome.vue
@@ -3,9 +3,11 @@
     <h2>Todo Page</h2>
   </div>
   <v-container fluid>
+    
       <v-card
         v-for="task in tasks"
         :key="task.id"
+        :class="{ 'indigo lighten-5': task.done }"
         border
         class="mb-2"
         density="compact"
@@ -13,15 +15,22 @@
         v-bind:title="task.taskTitle"
         variant="text"
       >
-        <v-img v-bind:src="task.photoImages" height="128" cover></v-img>
-
         <v-card-text>{{ task.textComents }}</v-card-text>
+        <v-img v-bind:src="task.photoImages" height="128" cover></v-img>
+        <v-btn color="primary" variant="text">View More</v-btn>            
 
         <template v-slot:actions>
-          <v-btn color="primary" variant="text">View More</v-btn>
-          <v-btn color="primary" variant="text">Complete</v-btn>
-          <v-btn color="primary" variant="text">Delete</v-btn>
-          <!-- <v-card-text>{{ new Date() }}</v-card-text> -->
+          <v-card @click="doneTask(task.taskId)">
+            <div class="d-flex justify-end">
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+              <v-btn color="primary" variant="text">Complete</v-btn>
+              <v-btn color="primary" variant="text">Delete</v-btn>
+              <!-- <v-card-text>{{ new Date() }}</v-card-text> -->
+            </div>
+          </v-card>
         </template>
       </v-card>
     </v-container>
@@ -68,23 +77,33 @@
           taskTitle:"Wake up",
           subTitle: "It is starting in a Good day",
           photoImages: "https://picsum.photos/512/128?image=660",
-          textComents: "If you wake up earliy, you get something you unexpected."
+          textComents: "If you wake up earliy, you get something you unexpected.",
+          done: false
         },
         {
           taskId: 2,
           taskTitle:"Study AWS",
           subTitle: "you should study cloudService in AWS",
           photoImages: "https://picsum.photos/512/128?random",
-          textComents: "you look around, CloudService run anywhere."
+          textComents: "you look around, CloudService run anywhere.",
+          done: true
         },
         {
           taskId: 3,
           taskTitle:"Program in TypeScript",
           subTitle: "It is important to keep typping in laung",
           photoImages: "https://picsum.photos/512/128?image=660",
-          textComents: "you code in Vue, you must undersatand it in Typescript."
+          textComents: "you code in Vue, you must undersatand it in Typescript.",
+          done: true
         },
       ]
     }),
+    methods: {
+      doneTask(id) {
+        let task = this.tasks.filter(task => task.taskId === id)[0]
+        task.done = !task.done
+        // console.log(taskId);
+      }
+    }
   }
 </script>
