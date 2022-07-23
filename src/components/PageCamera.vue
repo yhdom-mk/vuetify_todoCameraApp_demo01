3f98f71a9465f62d08b09905b90cdb84c82bca40
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-23 19:22:59 +0900

    img camera init機能追加

--- src/components/PageCamera.vue
+++ src/components/PageCamera.vue
@@ -6,11 +6,16 @@
     <v-row>
       <v-col cols="12">
         <v-card>
-            <v-img
+          <video
+            ref="video"
+            class="full-width"
+            autoplay
+          ></video>
+            <!-- <v-img
               src="https://picsum.photos/350/165?random"
               height="300"
               class="camera-frame"
-            ></v-img>
+            ></v-img> -->
           </v-card>
       </v-col>
     </v-row>
@@ -61,6 +66,34 @@
     class="bg-grey-lighten-1"
   ></v-footer>
 </template>
+
+<script>
+export default({
+  methods: {
+    initCamera() {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.$refs.video.srcObject = stream
+      })
+      // console.log('init Camera');
+    }
+  },
+  mounted() {
+    this.initCamera()
+  }
+  // setup() {
+  //   const initCamera = () => {
+  //     console.log('init camera');
+  //   }
+  //   return { initCamera }
+  // },
+  // onMounted() {
+
+  // }
+})
+</script>
+
 
 <style lang="sass">
   .camera-frame
