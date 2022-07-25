67bc147a2f0f584e0f3c4900510e9a52a060d2b8
Mski Ydom <lcydome.msk12@gmail.com>
2022-07-25 13:48:06 +0900

    dataURI to Blob, cameraSupport 機能追加

--- src/components/PageCamera.vue
+++ src/components/PageCamera.vue
@@ -27,13 +27,22 @@
       </v-col>
     </v-row>
   </v-container>
-  <div class="d-flex justify-space-around align-center">
+  <div class="d-flex justify-space-around align-center pa-md">
     <v-btn
+      v-if="hasCameraSupport"
       @click="captureImage"
       color=#E0E0E0
       icon="mdi-camera"
     ></v-btn>
   </div>
+  <v-file-input
+    v-if="!hasCameraSupport"
+    v-model="imageUpload"
+    @input="captureImageFallback"
+    class="px-6"
+    accept="image/*"
+    label="Choose an Image File"
+  ></v-file-input>
   <div class="px-4">
     <v-text-field
       class="pa-3"
@@ -92,7 +101,9 @@ export default {
       photo: null,
       date: Date.now()
     },
-    imageCaptured: false
+    imageCaptured: false,
+    imageUpload: [],
+    hasCameraSupport: true
   }),
   methods: {
     initCamera() {
@@ -100,6 +111,8 @@ export default {
         video: true
       }).then(stream => {
         this.$refs.video.srcObject = stream
+      }).catch( ()=> {
+        this.hasCameraSupport = false
       })
       // console.log('init Camera');
     },
@@ -111,6 +124,30 @@ export default {
       let context = canvas.getContext('2d')
       context.drawImage(video, 0, 0, canvas.width, canvas.height)
       this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+    },
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = window.atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+          ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], {type: mimeString});
+      return blob;
     }
   },
   mounted() {
