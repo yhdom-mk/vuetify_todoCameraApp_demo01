4a0242141ae8dc39ed08efd7eaa1fcda14aa0327
Mski Ydom <lcydome.msk12@gmail.com>
2022-08-02 17:26:38 +0900

    posts 仮実装

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('I move on Node.js a single day.')
+app.get('/posts', (req, res) => {
+  // res.send('I move on Node.js a single day.')
+  let posts = [
+    {
+      photoImages: "https://picsum.photos/512/128?image=660",
+      textComents: "If you wake up earliy, you get something you unexpected.",
+    },
+    {
+      photoImages: "https://picsum.photos/512/128?image=660",
+      textComents: "If you wake up earliy, you get something you unexpected.",
+    }
+  ]
+  res.send(posts)
 })
 
 app.listen(port, () => {
