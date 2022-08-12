8469bb1dc0b9953c87fd7cdd82aec41c8ad1466f
Mski Ydom <lcydome.msk12@gmail.com>
2022-08-13 02:13:20 +0900

    busboy postfile機能　仮実装

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
-const express = require('express')
-const busboy = require('connect-busboy')
+const express = require('express');
+const busboy = require('connect-busboy');
+let path = require('path');
+let os = require('os');
+let fs = require('fs');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,7 +19,39 @@ app.get('/posts', (req, res) => {
       textComents: "If you wake up earliy, you get something you unexpected.",
     }
   ]
+  // res.set('Access=Control-Allow-Origin', '*')
+  // db.collection('posts').orderBy().get().then(snapshot => {
+  //   posts.push(doc.data())
+  // });
   res.send(posts)
+})
+
+app.post('/createPost', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+
+  let busboy = new Busboy({ Headers: req.headers })
+  let field = {}
+  busboy.on('file', function(fieldname, filename, file, encoding, mimetype) {
+    console.log('File[' + fieldname + ']: filename:' + filename + 'encoding:' + encoding + 'mimetype:' + mimetype);
+    file.on('data', function(data) {
+      console.log('data:' + data)
+    })
+    file.on('end', function() {
+      console.log('Finished:')
+    })
+  });
+  busboy.on('field', function(fieldname, val, filename) {
+    console.log('Field' + fieldname + 'value:' + inspect(val));
+    field[filename] = val
+  })
+  busboy.on('finish', function(fieldname, val) {
+    // db.collection('posts', doc(field.id).set({
+    //   id: field.id,
+    //   imageurl: ''
+    // }))
+    res.writeHead(303, { Connection: 'close' })
+    res.end()
+  })
 })
 
 app.use(busboy())
