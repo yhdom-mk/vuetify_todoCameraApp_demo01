a71be7e00808b20e20e8e76824c5ff0789cfa23c
Mski Ydom <lcydome.msk12@gmail.com>
2022-08-16 14:20:10 +0900

    busboy buket, filedata 仮実装

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const busboy = require('connect-busboy');
 let path = require('path');
 let os = require('os');
 let fs = require('fs');
+const { uuid } = require('vue-uuid');
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// const db = admin.firestore();
+// let bucket = admin.storage().bucket();
 
 app.get('/posts', (req, res) => {
   // res.send('I move on Node.js a single day.')
@@ -31,8 +35,15 @@ app.post('/createPost', (req, res) => {
 
   let busboy = new Busboy({ Headers: req.headers })
   let field = {}
+  let fileData = {}
+
   busboy.on('file', function(fieldname, filename, file, encoding, mimetype) {
     console.log('File[' + fieldname + ']: filename:' + filename + 'encoding:' + encoding + 'mimetype:' + mimetype);
+    
+    let filepath = path.join(os.tmpdir(), filename)
+    file.pipe(fs.createWriteStream(filepath))
+    fileData = { filepath, mimetype }
+    
     file.on('data', function(data) {
       console.log('data:' + data)
     })
@@ -45,10 +56,32 @@ app.post('/createPost', (req, res) => {
     field[filename] = val
   })
   busboy.on('finish', function(fieldname, val) {
-    // db.collection('posts', doc(field.id).set({
-    //   id: field.id,
-    //   imageurl: ''
-    // }))
+
+    bucket.upload(fileData.filepath, {
+      uploadType: 'media',
+      metadata: {
+        metedata: {
+          contentType: fileData.mimetype,
+          firebaseStorageDownloadTokens: uuid
+        }
+      }
+    },
+    (err, uploadedFile) => {
+      if(!err) {
+        createDocument()
+      }
+    })
+
+    function createDocument() {
+      db.collection('posts').doc(field.id).set({
+        id: field.id,
+        imageurl: ''
+      }).then(res => {
+        res.send('Post added:' + field.id)
+      })
+    }
+    // res.send('Done parsing form.')
+    
     res.writeHead(303, { Connection: 'close' })
     res.end()
   })
